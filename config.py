6485d1f8cf97ac669eaaed7420a896ff24ea0efb
Thomas Capelle <tcapelle@pm.me>
2025-05-15 11:52:34 +0000

    run name

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ import trl
 class GRPOScriptArguments(trl.ScriptArguments):
     dataset_name: str = "gsm8k_dataset_prepared"
     model_name: str = "Qwen/Qwen3-4B"
-    wandb_project: str = "grpo_weave"
-    wandb_entity: Optional[str] = "grpo-cuda"
+    wandb_project: str = "simple_grpo"
+    wandb_entity: Optional[str] = None
+    wandb_run_name: Optional[str] = None
